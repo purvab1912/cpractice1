0051570c91f77b86ab5c36e4db858d13262d3d04
purva bhokare <purvab08@gmail.com>
2023-01-13 15:06:32 +0530

    first commit

---
+++ cpractice.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+    int a;
+    float b;
+    double c;
+    char d;
+    //sizeof eavaluates the size of variable
+    printf ("size of int:%d bytes\n",sizeof a);
+    printf ("size of float:%d bytes\n",sizeof b);
+    printf ("size of double:%d bytes\n",sizeof c);
+    printf ("size of char:%d bytes\n",sizeof d);   
+    return 0;
+    
+}
